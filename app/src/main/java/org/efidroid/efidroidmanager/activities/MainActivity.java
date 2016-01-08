cc4c9a71219661738604ced46d7ca8deb4cef50b
M1cha <sigmaepsilon92@gmail.com>
2016-01-08 22:01:02 +0100

    use com.melnykov.fab.FloatingActionButton

--- app/src/main/java/org/efidroid/efidroidmanager/activities/MainActivity.java
+++ app/src/main/java/org/efidroid/efidroidmanager/activities/MainActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
+import com.melnykov.fab.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
