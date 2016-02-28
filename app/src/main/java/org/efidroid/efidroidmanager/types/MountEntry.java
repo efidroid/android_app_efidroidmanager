1ea7239a0f6017ad4972f7e44d41df1f303be7b3
M1cha <sigmaepsilon92@gmail.com>
2016-02-28 19:59:40 +0100

    make MountEntry parcelable

--- app/src/main/java/org/efidroid/efidroidmanager/types/MountEntry.java
+++ app/src/main/java/org/efidroid/efidroidmanager/types/MountEntry.java
@@ -1,6 +1,9 @@
 package org.efidroid.efidroidmanager.types;
 
-public class MountEntry {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class MountEntry implements Parcelable {
     private final int mMountID;
     private final int mParentID;
     private final int mMajor;
@@ -75,5 +78,53 @@ public class MountEntry {
 
     public String getSuperOptions() {
         return mSuperOptions;
+    }
+
+    protected MountEntry(Parcel in) {
+        mMountID = in.readInt();
+        mParentID = in.readInt();
+        mMajor = in.readInt();
+        mMinor = in.readInt();
+        mRoot = in.readString();
+        mMountPoint = in.readString();
+        mMountOptions = in.readString();
+        mOptionalFields = in.readString();
+        mSeparator = in.readString();
+        mFsType = in.readString();
+        mMountSource = in.readString();
+        mSuperOptions = in.readString();
+    }
+
+    public static final Creator<MountEntry> CREATOR = new Creator<MountEntry>() {
+        @Override
+        public MountEntry createFromParcel(Parcel in) {
+            return new MountEntry(in);
+        }
+
+        @Override
+        public MountEntry[] newArray(int size) {
+            return new MountEntry[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(mMountID);
+        dest.writeInt(mParentID);
+        dest.writeInt(mMajor);
+        dest.writeInt(mMinor);
+        dest.writeString(mRoot);
+        dest.writeString(mMountPoint);
+        dest.writeString(mMountOptions);
+        dest.writeString(mOptionalFields);
+        dest.writeString(mSeparator);
+        dest.writeString(mFsType);
+        dest.writeString(mMountSource);
+        dest.writeString(mSuperOptions);
     }
 }
